8d64adf1374966e3c6d528f6c157051453774cca
Charles Kang <kscscy@gmail.com>
2020-07-01 19:05:38 +0900

    Pre-rendering and Data Fetching

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 
 /*
 import Link from 'next/link'
@@ -219,7 +220,7 @@ export default function Home() {
 }
 */
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -232,6 +233,33 @@ export default function Home() {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   )
+}
+
+//This is possible because getStaticProps runs only on the server - side.
+//It will never be run on the client - side.
+//It won’t even be included in the JS bundle for the browser.
+//That means you can write code such as direct database queries without them being sent to browsers.
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
